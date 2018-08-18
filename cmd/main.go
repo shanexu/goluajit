c4695166ca8516426154dbe72bf82c7d4e06a244
Shane Xu <xusheng0711@gmail.com>
2018-08-18 11:13:27 +0800

    simple test

--- cmd/main.go
+++ cmd/main.go
@@ -1,10 +1,24 @@
 package main
 
 import (
+	"github.com/shanexu/goluajit"
+	"os"
 	"fmt"
-	_ "github.com/shanexu/goluajit"
 )
 
 func main() {
-	fmt.Println("hello world")
+	if len(os.Args) != 2 {
+		fmt.Errorf("main file.lua\n")
+		os.Exit(1)
+	}
+	file := os.Args[1]
+	_, err := os.Stat(file)
+	if err != nil {
+		panic(err)
+	}
+	s := goluajit.NewState()
+	s.LoadFile(file)
+	fmt.Println(s.PCall(0, 0, 0))
+	s.GetGlobal("address")
+	s.ToString(-1)
 }
---
+++ lua.go
@@ -0,0 +1,39 @@
+package goluajit
+
+// #include <stdio.h>
+// #include <lua.h>
+// #include <lualib.h>
+// #include <lauxlib.h>
+import "C"
+
+type LuaState struct {
+	ls *C.lua_State
+}
+
+func NewState() *LuaState {
+	ls := C.luaL_newstate()
+	s := &LuaState{
+		ls: ls,
+	}
+	return s
+}
+
+func (s *LuaState) OpenLibs() {
+	C.luaL_openlibs(s.ls)
+}
+
+func (s *LuaState) LoadFile(file string) {
+	C.luaL_loadfile(s.ls, C.CString(file))
+}
+
+func (s *LuaState) PCall(i1, i2, i3 int) int {
+	return int(C.lua_pcall(s.ls, C.int(i1), C.int(i2), C.int(i3)))
+}
+
+func (s *LuaState) GetGlobal(name string) {
+	C.lua_getfield(s.ls, C.LUA_GLOBALSINDEX, C.CString(name))
+}
+
+func (s *LuaState) ToString(i int) string {
+	return C.GoString(C.lua_tolstring(s.ls, C.int(i), nil))
+}
